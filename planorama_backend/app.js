5032c6b608636132391c39bfaa653ed59aafca0a
ARRY7686 <aadigupta2007@yahoo.com>
2025-04-21 00:36:54 +0530

    fix: update CORS configuration to allow all origins

--- planorama_backend/app.js
+++ planorama_backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 
 app.use('/', cityAndAirportSearch);
 app.use('/flights', flightSearch);
